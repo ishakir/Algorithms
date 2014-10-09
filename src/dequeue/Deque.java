f99e6fe425147b5a9dac4b1fe57a7e7a7a5f4c3e
ishakir <shakimra@uk.ibm.com>
2014-10-09 14:49:49 +0100

    Doubly Linked List implementation of deque

---
+++ src/dequeue/Deque.java
@@ -0,0 +1,14 @@
+package dequeue;
+
+public interface Deque<Item> {
+	
+	boolean isEmpty();
+	int size();
+	
+	void pushLeft(Item item);
+	void pushRight(Item item);
+	
+	Item popLeft();
+	Item popRight();
+	
+}
