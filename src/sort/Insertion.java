b3eb31d4c2b94d55b0f1dcc42d69959c471476fe
ishakir <shakimra@uk.ibm.com>
2014-10-14 14:55:17 +0100

    Tidy up insertion sort

--- src/sort/Insertion.java
+++ src/sort/Insertion.java
@@ -17,12 +17,5 @@ public class Insertion<Item extends Comparable<Item>> extends Sort<Item> {
 		}
 		return a;
 	}
-	
-	public static void main(String[] args) {
-		Integer[] intArray = {9,2,5,3,8,1,7,4,6};
-		Sort<Integer> sort = new Insertion<Integer>(intArray);
-		sort.show();
-		sort.sort();
-	}
 
 }
