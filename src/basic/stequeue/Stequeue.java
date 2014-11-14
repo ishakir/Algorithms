7314782cfd63ee0b9e18967eb6151caacc4aa181
ishakir <shakimra@uk.ibm.com>
2014-11-14 13:12:41 +0000

    Removed files from package renaming

--- src/basic/stequeue/Stequeue.java
+++
@@ -1,9 +0,0 @@
-package basic.stequeue;
-
-public interface Stequeue<Item> {
-	public void push(Item item);
-	public Item pop();
-	public void enqueue(Item item);
-	public boolean isEmpty();
-	public int size();
-}
